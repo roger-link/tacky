2aacedc1c6ee08836a1b2ebe61784203654acebf
Roger Link <roger.link@cerner.com>
2016-05-11 21:15:47 -0500

    add Coordinate and Board model, add start_game model method, add migration

--- tacky/games/models.py
+++ tacky/games/models.py
@@ -1,29 +1,42 @@
 # -*- coding: utf-8 -*-
+import random
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 from tacky.users import models as user_models
 
+ALL_SPOTS = [1,2,3,4,5,6,7,8,9]
 
+@python_2_unicode_compatible
+class Coordinate(models.Model):
+    coordinate = models.IntegerField()
+    player = models.ForeignKey(user_models.User)
+
+    def __str__(self):
+        return str(self.id)
+
+@python_2_unicode_compatible
+class Board(models.Model):
+    coordinates = models.ManyToManyField(Coordinate)
+    current_player = models.ForeignKey(user_models.User)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_move(self):
+        board_spots = [x.coordinate for x in self.coordinates.all()]
+        free_spots = list(set(ALL_SPOTS) - set(board_spots))
+        return random.choice(free_spots)
 
 @python_2_unicode_compatible
 class Game(models.Model):
 
     player1 = models.ForeignKey(user_models.User, related_name='player1')
     player2 = models.ForeignKey(user_models.User, related_name='player2')
-    current_player = models.ForeignKey(user_models.User, null=True, blank=True, related_name='current_user')
     active = models.BooleanField(default=True)
     winner =  models.ForeignKey(user_models.User, null=True, blank=True, related_name='winner')
-
-    one = models.ForeignKey(user_models.User, null=True, blank=True, related_name='one')
-    two = models.ForeignKey(user_models.User, null=True, blank=True, related_name='two')
-    three = models.ForeignKey(user_models.User, null=True, blank=True, related_name='three')
-    four = models.ForeignKey(user_models.User, null=True, blank=True, related_name='four')
-    five = models.ForeignKey(user_models.User, null=True, blank=True, related_name='five')
-    six = models.ForeignKey(user_models.User, null=True, blank=True, related_name='six')
-    seven = models.ForeignKey(user_models.User, null=True, blank=True, related_name='seven')
-    eight = models.ForeignKey(user_models.User, null=True, blank=True, related_name='eight')
-    nine = models.ForeignKey(user_models.User, null=True, blank=True, related_name='nine')
+    board = models.ForeignKey(Board, related_name='board', null=True)
 
     def __str__(self):
         return str(self.id)
--- tacky/games/views.py
+++ tacky/games/views.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Game
+from tacky.users import models as u_models
+from tacky.games import models as g_models
 
 
 class GameView(LoginRequiredMixin, TemplateView):
@@ -15,4 +14,30 @@ class GameView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(GameView, self).get_context_data(**kwargs)
+        game = self.start_game()
+        context['game'] = game
         return context
+
+    def start_game(self):
+    	computer = u_models.User.objects.get(name='computer')
+
+    	coordinate1 = g_models.Coordinate(player=self.request.user, coordinate=1)
+        coordinate1.save()
+        coordinate3 = g_models.Coordinate(player=self.request.user, coordinate=3)
+        coordinate3.save()
+
+    	board = g_models.Board(current_player=self.request.user)
+        board.save()
+        board.coordinates.add(coordinate1, coordinate3)
+        board.save()
+
+
+        game = g_models.Game.objects.create(
+        	player1 = self.request.user,
+        	player2 = computer,
+        	board = board
+        	)
+
+
+    	return game
+
